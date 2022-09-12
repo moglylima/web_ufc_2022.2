487a10f3ff7f1088ba403823082382e11cb1c0cf
Your Name <you@example.com>
2022-09-12 14:46:12 -0300

    chore(Calculadora.jsx): Ajustando componente.

--- aulas/aula_03/project-01/src/App.js
+++ aulas/aula_03/project-01/src/App.js
@@ -1,15 +1,15 @@
 import React, { Component } from "react";
 import "./App.css";
-// import { Calculadora } from './components/Calculadora';
+import { Calculadora } from './components/Calculadora';
 import { IMC } from "./components/IMC";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        {/* <Calculadora op={"SOMA"} x={10} y={29} /> */}
+        <Calculadora op={"multiplicacao"} x={10} y={29} />
 
-        <IMC peso={82} altura={1.82} />
+        {/* <IMC peso={82} altura={1.82} /> */}
       </div>
     );
   }
--- aulas/aula_03/project-01/src/components/Calculadora.jsx
+++ aulas/aula_03/project-01/src/components/Calculadora.jsx
@@ -1,26 +1,51 @@
 import React from "react";
 
-export function Calculadora({op, x, y}){
-    
-    function soma(){
+export function Calculadora({ op, x, y }) {
+
+    function soma() {
         return x + y;
     }
 
-    function subtracao(){
+    function subtracao() {
         return x - y;
     }
 
-    function divisao(){
+    function divisao() {
         return x / y;
     }
 
-    function multiplicacao(){
+    function multiplicacao() {
         return x * y;
     }
-    
-    return(
+
+    function calculo() {
+        if (op === "soma") {
+            return (
+                <h1>Resultado: {soma(x, y)}</h1>
+            )
+        }
+        if (op === "subtracao") {
+            return (
+                <h1>Resultado: {subtracao(x, y)}</h1>
+            )
+        }
+        if (op === "multiplicacao") {
+            return (
+                <h1>Resultado: {multiplicacao(x, y)}</h1>
+            )
+        }
+        if (op === "divisao") {
+            return (
+                <h1>Resultado: {divisao(x, y).toFixed(2)}</h1>
+            )
+        }return(
+            <h1>Operação invalida!!!</h1>
+        )
+    }
+
+    return (
         <div>
-            
+            {calculo()}
         </div>
     )
 }
