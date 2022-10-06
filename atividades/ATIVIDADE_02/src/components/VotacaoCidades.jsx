36fc0b22aea7fd77983db69edd74ca54cfce38d9
Mogleson Lima <moglesonlima@gmail.com>
2022-10-06 02:14:36 -0300

    Tentando arrumar a bagunça

--- atividades/ATIVIDADE_02/src/components/VotacaoCidades.jsx
+++ atividades/ATIVIDADE_02/src/components/VotacaoCidades.jsx
@@ -1,122 +1,163 @@
-import React, {  useState } from "react";
+import React, { useState } from "react";
 
 const VotacaoCidades = () => {
+  // Votação de Cidades
+  // Cidade melhor votada
 
-    // Votação de Cidades
-    // Cidade melhor votada
+  const [quixada, setQuixada] = useState(0);
+  const [banabuiu, setBanabuiu] = useState(0);
+  const [quixeramobim, setQuixeramobim] = useState(0);
 
-    const [quixada, setQuixada] = useState(0)
-    const [banabuiu, setBanabuiu] = useState(0)
-    const [quixeramobim, setQuixeramobim] = useState(0)
+  const cidadesStr = ["Quixada", "Banabuiu", "Quixeramobim"];
+  const cidades = [quixada, banabuiu, quixeramobim];
 
-    const cidadesStr = ["Quixada", "Banabuiu", "Quixeramobim"]
-    const cidades = [quixada, banabuiu, quixeramobim]
+  function computarVotacao() {
+    let maior = 0;
+    let nameCidades = [];
 
-    function computarVotacao() {
-        let maior = 0
-        let nameCidades = []
-
-        for(let i = 0; i < cidades.length; i++){
-            if(cidades[i] > maior){
-                maior = cidades[i]
-                nameCidades = []
-            }
-            if(cidades[i] === maior){
-                nameCidades.push(cidadesStr[i])
-            }
-        }
-        //Retornamos um obj. contendo um array com as cidades e o número de votos.
-        return {nameCidades, maior};
+    for (let i = 0; i < cidades.length; i++) {
+      if (cidades[i] > maior) {
+        maior = cidades[i];
+        nameCidades = [];
+      }
+      if (cidades[i] === maior) {
+        nameCidades.push(cidadesStr[i]);
+      }
     }
-    
-    function mostreResultado(){
-        //Aqui chamamos a função que computa a votação(guardamos o retorno em uma constante)
-        let cityQtdVotos = computarVotacao()
+    //Retornamos um obj. contendo um array com as cidades e o número de votos.
+    return { nameCidades, maior };
+  }
 
-        //Aqui desestruturamos o objeto que recebemos da função computarVotacao
-        let nameCidades = cityQtdVotos.nameCidades,
-        qtdVotos = cityQtdVotos.maior
+  function mostreResultado() {
+    //Aqui chamamos a função que computa a votação(guardamos o retorno em uma constante)
+    let cityQtdVotos = computarVotacao();
 
-        //Aqui verificamos se houve algum voto
-        if(qtdVotos === 0){
-            return alert("Nenhuma cidade foi votada!");
-        }
-        // Aqui verificamos quem foi a cidade mais votada
-        if(nameCidades.length === 1){
-            return alert("A cidade com mais votos é " + nameCidades + " com " + qtdVotos + " voto(s)");
-        }
-        //Aqui tratamos o empate
-        if(nameCidades.length > 1){
-            return alert("As cidades com mais votos são " + nameCidades + " com " + qtdVotos + " voto(s)");
-        }
-        
+    //Aqui desestruturamos o objeto que recebemos da função computarVotacao
+    let nameCidades = cityQtdVotos.nameCidades,
+      qtdVotos = cityQtdVotos.maior;
+
+    //Aqui verificamos se houve algum voto
+    if (qtdVotos === 0) {
+      return alert("Nenhuma cidade foi votada!");
     }
-
-    function reset(){
-        setQuixada(0)
-        setBanabuiu(0)
-        setQuixeramobim(0)
+    // Aqui verificamos quem foi a cidade mais votada
+    if (nameCidades.length === 1) {
+      return alert(
+        "A cidade com mais votos é " +
+          nameCidades +
+          " com " +
+          qtdVotos +
+          " voto(s)"
+      );
     }
+    //Aqui tratamos o empate
+    if (nameCidades.length > 1) {
+      return alert(
+        "As cidades com mais votos são " +
+          nameCidades +
+          " com " +
+          qtdVotos +
+          " voto(s)"
+      );
+    }
+  }
 
-    return (
-        <div className="container">
-            <table className="table">
-                <thead>
-                    <tr><th colSpan={4}>Votação Cidades</th></tr>
-                    <tr>
-                        <th scope="col"></th>
-                        <th scope="col">Cidades</th>
-                        <th scope="col">Qtd. Votos</th>
-                        <th scope="col">Votar</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <tr>
-                        <th scope="row">1</th>
-                        <td>Quixada</td>
-                        <td>{quixada}</td>
-                        <td>
-                            <button type="button" className="btn btn-primary"
-                                onClick={() => setQuixada(quixada + 1)}
-                            >Votar</button>
-                        </td>
-                    </tr>
-                    <tr>
-                        <th scope="row">2</th>
-                        <td>Quixeramobim</td>
-                        <td>{quixeramobim}</td>
-                        <td>
-                            <button type="button" className="btn btn-primary"
-                                onClick={() => setQuixeramobim(quixeramobim + 1)}
-                            > Votar</button>
-                        </td>
-                    </tr>
-                    <tr>
-                        <th scope="row">3</th>
-                        <td>Banabuiu</td>
-                        <td>{banabuiu}</td>
-                        <td>
-                            <button type="button" className="btn btn-primary"
-                                onClick={() => setBanabuiu(banabuiu + 1) }
-                            >Votar</button>
-                        </td>
-                    </tr>
-                </tbody>
-            </table>
+  function reset() {
+    setQuixada(0);
+    setBanabuiu(0);
+    setQuixeramobim(0);
+  }
 
-            <div >
-                <button type="button" className="btn btn-sm btn-dark" onClick={mostreResultado}>
-                    Resultado
-                </button>
+  //   Aprimorando
 
-                <button type="button" className="btn btn-sm btn-warning" onClick={reset}>
-                    Resetar Votação
-                </button>
-            </div>
+  return (
+    <div className="container">
+      <table className="table">
+        <thead>
+          <tr>
+            <th colSpan={4}>Votação Cidades</th>
+          </tr>
+          <tr>
+            <th scope="col"></th>
+            <th scope="col">Cidades</th>
+            <th scope="col">Qtd. Votos</th>
+            <th scope="col">Votar</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <th scope="row">1</th>
+            <td>Quixada</td>
+            <td>{quixada}</td>
+            <td>
+              <button
+                type="button"
+                className="btn btn-primary"
+                onClick={() => setQuixada(quixada + 1)}
+              >
+                Votar
+              </button>
+            </td>
+          </tr>
+          <tr>
+            <th scope="row">2</th>
+            <td>Quixeramobim</td>
+            <td>{quixeramobim}</td>
+            <td>
+              <button
+                type="button"
+                className="btn btn-primary"
+                onClick={() => setQuixeramobim(quixeramobim + 1)}
+              >
+                {" "}
+                Votar
+              </button>
+            </td>
+          </tr>
+          <tr>
+            <th scope="row">3</th>
+            <td>Banabuiu</td>
+            <td>{banabuiu}</td>
+            <td>
+              <button
+                type="button"
+                className="btn btn-primary"
+                onClick={() => setBanabuiu(banabuiu + 1)}
+              >
+                Votar
+              </button>
+            </td>
+          </tr>
+        </tbody>
+      </table>
 
+      <div>
+        <button
+          type="button"
+          className="btn btn-sm btn-dark"
+          onClick={mostreResultado}
+        >
+          Resultado
+        </button>
 
+        <button
+          type="button"
+          className="btn btn-sm btn-warning"
+          onClick={reset}
+        >
+          Resetar Votação
+        </button>
+      </div>
 
-        </div>)
-}
+      <form onSubmit={null}>
+        <label>
+          Nome:
+          <input type="text" value={null} onChange={null} />
+        </label>
+        <input type="submit" value="Enviar" />
+      </form>
+    </div>
+  );
+};
 
-export default VotacaoCidades
+export default VotacaoCidades;
--- aulas/aula_07/src/components/student/CreateStudent.jsx
+++ aulas/aula_07/src/components/student/CreateStudent.jsx
@@ -2,10 +2,9 @@ import React from "react";
 import { useState } from "react";
 
 const CreateStudent = () => {
-
-  const [name, setName] = useState('')
-  const [course, setCourse] = useState('')
-  const [ira, setIra] = useState(0.0)
+  const [name, setName] = useState("");
+  const [course, setCourse] = useState("");
+  const [ira, setIra] = useState(0.0);
 
   // id: 3, name: "Vilnei", course: "DD", ira: 4.5 }
   const handleSubmit = (event) => {
@@ -16,70 +15,60 @@ const CreateStudent = () => {
     //   course: course,
     //   ira: ira
     // }
-    console.log(name)
-    console.log(course)
-    console.log(ira)
-  }
+    console.log(name);
+    console.log(course);
+    console.log(ira);
+  };
   return (
     <div style={{ marginTop: 20 }}>
       <h1>Create Student...</h1>
       <form onSubmit={handleSubmit}>
-        <div className='form-group'>
+        <div className="form-group">
           <label>Nome: </label>
           <input
-            type='text'
-            className='form-control'
-            placeholder='Digite seu nome'
-            onChange={
-              (event) => {
-                setName(event.target.value)
-              }
-            }
+            type="text"
+            className="form-control"
+            placeholder="Digite seu nome"
+            onChange={(event) => {
+              setName(event.target.value);
+            }}
           />
         </div>
-        <div className='form-group'>
+        <div className="form-group">
           <label>Curso: </label>
           <input
-            type='text'
-            className='form-control'
-            placeholder='Digite seu curso'
-            onChange={
-              (event) => {
-                setCourse(event.target.value)
-              }
-            }
+            type="text"
+            className="form-control"
+            placeholder="Digite seu curso"
+            onChange={(event) => {
+              setCourse(event.target.value);
+            }}
           />
         </div>
-        <div className='form-group'>
+        <div className="form-group">
           <label>IRA: </label>
           <input
-            type='numeric'
+            type="numeric"
             min="0"
             max="10"
-            step='any'
-            className='form-control'
-            placeholder='Digite seu IRA'
-            onChange={
-              (event) => {
-                setIra(Number(event.target.value))
-              }
-            }
+            step="any"
+            className="form-control"
+            placeholder="Digite seu IRA"
+            onChange={(event) => {
+              setIra(Number(event.target.value));
+            }}
           />
         </div>
-        <div className='form-group' style={{ marginTop: 15 }}>
+        <div className="form-group" style={{ marginTop: 15 }}>
           <input
-            type='submit'
-            value='Criar Estudante'
-            className='btn btn-primary'
+            type="submit"
+            value="Criar Estudante"
+            className="btn btn-primary"
           />
         </div>
       </form>
     </div>
-
-
-
   );
-}
+};
 
 export default CreateStudent;
->>>>>>> 0c8b945b59ada24fe0b468780b5dac0d95b6b7a7
